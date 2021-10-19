45b884b9c392b3a1bc5ace5c0cbd897e15636078
Paulo Romeo Feliciano <pauloromeo_feliciano@yahoo.com>
2021-10-19 22:42:00 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
 function App() {
+  const [input, setInput] = React.useState("");
+  const [output, setOutput] = React.useState("");
+
+  const handleInputChange = (value) => {
+    if (value) {
+      if (value.includes(",")) {
+        const output = [];
+        let values = value.split(",").filter((e) => e);
+
+        values.forEach((value) => {
+          output.push(parseInt(value) * 2);
+        });
+        setOutput(output.toString());
+      } else {
+        setOutput(value * 2);
+      }
+    } else {
+      setOutput("");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Medirecords Front end Code Challenge</h1>
+      <h2>Please try to implement following within one hour</h2>
+
+      <div className="Container">
+        <div className="Box">
+          <div className="Content">
+            <strong>Input</strong>
+
+            <p>Array</p>
+            <input
+              type="text"
+              onChange={(e) => handleInputChange(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="Box">
+          <div className="Content">
+            <strong>Output</strong>
+
+            <p>Double</p>
+            <input type="text" value={output} readOnly />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
